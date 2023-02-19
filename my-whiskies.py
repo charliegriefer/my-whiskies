0dde2769ac1b7c7ac112a60d0bf83488d91e5fdc
charlie-partnerup <87634299+charlie-partnerup@users.noreply.github.com>
2023-02-19 12:25:04 -0700

    Added configs for prod environment

--- my-whiskies.py
+++ my-whiskies.py
@@ -1,9 +1,9 @@
 from app import create_app, db
 from app.models.user import User
 
-app = create_app()
+application = create_app()
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User}
