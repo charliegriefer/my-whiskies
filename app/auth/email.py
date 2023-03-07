d395c14d992e9bc463b57b60fb6bc4d628e8684c
Charlie Griefer <charlie@griefer.com>
2023-03-06 19:46:40 -0700

    For #35 - fixed copy/paste error

--- app/auth/email.py
+++ app/auth/email.py
@@ -4,7 +4,7 @@ from app.email import send_email
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email("[Partner Up with Me] Reset Your Password",
+    send_email("[My Whiskies Online] Reset Your Password",
                sender=current_app.config["MAIL_SENDER"],
                recipients=[user.email],
                text_body=render_template("email/reset_password.txt", user=user, token=token),
