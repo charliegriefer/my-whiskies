fe432a272707f08319fc014ccdac47e97119ecc1
Charlie Griefer <charlie@griefer.com>
2023-03-06 19:47:45 -0700

    For #35 - send email on 500 error

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,6 +1,7 @@
 from flask import current_app, render_template, request
 
 from app import db
+from app.email import send_email
 from app.errors import errors_blueprint
 
 
@@ -14,5 +15,9 @@ def not_found_error(error):
 @errors_blueprint.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    current_app.logger.debug(f"The 500 handler. {error}")
+    send_email("My Whiskies Online ERROR",
+               sender=current_app.config["MAIL_SENDER"],
+               recipients=current_app.config["MAIL_ADMINS"],
+               text_body=render_template("email/error.txt", error=error),
+               html_body=render_template("email/error.html", error=error))
     return render_template("errors/500.html"), 500
