5b830bbdeda48e04bd28958834b3de7ef9580525
Charlie Griefer <charlie@griefer.com>
2024-08-18 18:44:15 -0700

    For #129 - removed attachment option from email code.

--- app/email.py
+++ app/email.py
@@ -1,13 +1,15 @@
+from smtplib import SMTPRecipientsRefused
+
 from flask_mail import Message
 
 from app import mail
 
 
-def send_email(subject, sender, recipients, text_body, html_body, attachments=None):
+def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    if attachments:
-        for attachment in attachments:
-            msg.attach(*attachment)
-    mail.send(msg)
+    try:
+        mail.send(msg)
+    except SMTPRecipientsRefused:
+        pass
