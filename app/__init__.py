f5c7e5a1ef59ad9cbcf9363219e0355fc65d74c9
Charlie Griefer <charlie@griefer.com>
2024-09-02 12:55:42 -0700

    For #136 - renamed var from 'application' to 'app'... for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 import os
-from datetime import datetime
 
 import flask
 from dotenv import load_dotenv
@@ -7,15 +6,14 @@ from flask import Flask
 
 from logging.config import dictConfig
 from app.extensions import db, login_manager, mail, migrate
-from app.models import User
 
 dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")
 load_dotenv(dotenv_path=dotenv_path, verbose=True)
 
 
 def create_app():
-    application = Flask(__name__)
-    application.config.from_object(os.environ["CONFIG_TYPE"])
+    app = Flask(__name__)
+    app.config.from_object(os.environ["CONFIG_TYPE"])
 
     dictConfig(
         {
@@ -44,42 +42,34 @@ def create_app():
             "loggers": {
                 "": {
                     "handlers": ["console", "file"],
-                    "level": application.config["LOG_LEVEL"],
+                    "level": app.config["LOG_LEVEL"],
                 }
             }
         }
     )
 
-    @application.before_request
+    @app.before_request
     def before_request():
         flask.session.permanent = True
-        application.permanent_session_lifetime = application.permanent_session_lifetime
+        app.permanent_session_lifetime = app.permanent_session_lifetime
         flask.session.modified = True
 
-    db.init_app(application)
+    db.init_app(app)
 
-    login_manager.init_app(application)
+    login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     login_manager.login_message = "Please log in to access this page."
 
-    mail.init_app(application)
-    migrate.init_app(application, db)
+    mail.init_app(app)
+    migrate.init_app(app, db)
 
     # register blueprints
     from app.auth import auth_blueprint
     from app.errors import errors_blueprint
     from app.main import main_blueprint
 
-    application.register_blueprint(auth_blueprint, url_prefix="/auth")
-    application.register_blueprint(errors_blueprint)
-    application.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
+    app.register_blueprint(errors_blueprint)
+    app.register_blueprint(main_blueprint)
 
-
-def set_contexts(app):
-    @app.shell_context_processor
-    def make_shell_context():
-        return {"db": db, "User": User}
-
-    @app.context_processor
-    def inject_today_date():
-        return {"current_date": datetime.today()}
+    return app
