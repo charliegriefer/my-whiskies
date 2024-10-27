1cf7b772476518b5165b495372a8d56e5c5b35e6
Charlie Griefer <charlie@griefer.com>
2024-10-27 16:04:18 -0700

    For #145 - removed handler file.

--- mywhiskies/blueprints/bottle/views.py
+++ mywhiskies/blueprints/bottle/views.py
@@ -1,8 +1,11 @@
+import io
 import random
 import time
 from datetime import datetime
+from typing import Union
 
 import boto3
+from botocore.exceptions import ClientError
 from dateutil.relativedelta import relativedelta
 from flask import (
     Blueprint,
@@ -14,8 +17,8 @@ from flask import (
     url_for,
 )
 from flask_login import current_user, login_required
+from PIL import Image
 
-from mywhiskies import handler as main_handler
 from mywhiskies.blueprints.bottle.forms import BottleEditForm, BottleForm
 from mywhiskies.blueprints.bottle.models import Bottle, BottleTypes
 from mywhiskies.blueprints.distillery.models import Distillery
@@ -122,7 +125,7 @@ def bottle_detail(bottle_id: str):
 @bottle.route("/bottle/add", methods=["GET", "POST"])
 @login_required
 def bottle_add():
-    form = main_handler.prep_bottle_form(current_user, BottleForm())
+    form = prep_bottle_form(current_user, BottleForm())
 
     if request.method == "POST" and form.validate_on_submit():
         bottle_in = Bottle(user_id=current_user.id)
@@ -158,7 +161,7 @@ def bottle_add():
         flash_message = f'"{bottle_in.name}" has been successfully added.'
         flash_category = "success"
 
-        image_upload_success = main_handler.bottle_add_images(form, bottle_in)
+        image_upload_success = bottle_add_images(form, bottle_in)
 
         if image_upload_success:
             pass
@@ -168,7 +171,7 @@ def bottle_add():
             db.session.delete(bottle_in)
             db.session.commit()
 
-        bottle_in.image_count = main_handler.get_bottle_image_count(bottle_in.id)
+        bottle_in.image_count = get_bottle_image_count(bottle_in.id)
 
         db.session.commit()
 
@@ -186,7 +189,7 @@ def bottle_add():
 @login_required
 def bottle_edit(bottle_id: str):
     _bottle = db.get_or_404(Bottle, bottle_id)
-    form = main_handler.prep_bottle_form(current_user, BottleEditForm(obj=_bottle))
+    form = prep_bottle_form(current_user, BottleEditForm(obj=_bottle))
 
     if request.method == "POST" and form.validate_on_submit():
         _bottle.name = form.name.data
@@ -213,14 +216,14 @@ def bottle_edit(bottle_id: str):
         if _bottle.bottler_id == "0":
             _bottle.bottler_id = None
 
-        main_handler.bottle_edit_images(form, _bottle)
-        image_upload_success = main_handler.bottle_add_images(form, _bottle)
+        bottle_edit_images(form, _bottle)
+        image_upload_success = bottle_add_images(form, _bottle)
 
         if image_upload_success:
-            main_handler.bottle_delete_images(_bottle)
+            bottle_delete_images(_bottle)
             flash_message = f'"{_bottle.name}" has been successfully updated.'
             flash_category = "success"
-            _bottle.image_count = main_handler.get_bottle_image_count(_bottle.id)
+            _bottle.image_count = get_bottle_image_count(_bottle.id)
             db.session.add(_bottle)
             db.session.commit()
         else:
@@ -260,3 +263,123 @@ def bottle_delete(bottle_id: str):
     return redirect(
         url_for("bottle.list_bottles", username=current_user.username.lower())
     )
+
+
+def prep_bottle_form(
+    user, form: Union[BottleForm, BottleEditForm]
+) -> Union[BottleForm, BottleEditForm]:
+    # set up the bottle types dropdown
+    form.type.choices = [(t.name, t.value) for t in BottleTypes]
+    # sort the list and move "Other" to the bottom
+    form.type.choices.sort()
+    form.type.choices.append(
+        form.type.choices.pop(form.type.choices.index(("other", "Other")))
+    )
+    form.type.choices.insert(0, ("", "Choose a Bottle Type"))
+
+    # set up the distilleries dropdown
+    distilleries = user.distilleries
+    distilleries.sort(key=lambda d: d.name)
+    form.distilleries.choices = [(d.id, d.name) for d in distilleries]
+    form.distilleries.choices.insert(0, ("", "Choose One or More Distilleries"))
+    form.distilleries.choices.insert(1, ("", " "))
+
+    # set up the bottlers dropdown
+    bottlers = user.bottlers
+    bottlers.sort(key=lambda d: d.name)
+    form.bottler_id.choices = [(b.id, b.name) for b in bottlers]
+    form.bottler_id.choices.insert(0, (0, "Distillery Bottling"))
+
+    # set up the star rating dropdown
+    form.stars.choices = [(str(x * 0.5), str(x * 0.5)) for x in range(0, 11)]
+    form.stars.choices.insert(0, ("", "Enter a Star Rating (Optional)"))
+
+    return form
+
+
+def bottle_add_images(
+    form: Union[BottleForm, BottleEditForm], bottle_in: Bottle
+) -> bool:
+    for i in range(1, 4):
+        image_field = form[f"bottle_image_{i}"]
+
+        if image_field.data:
+            image_in = Image.open(image_field.data)
+            if image_in.width > 400:
+                divisor = image_in.width / 400
+                image_dims = (
+                    int(image_in.width / divisor),
+                    int(image_in.height / divisor),
+                )
+                image_in = image_in.resize(image_dims)
+
+            new_filename = f"{bottle_in.id}_{i}"
+
+            in_mem_file = io.BytesIO()
+            image_in.save(in_mem_file, format="png")
+            in_mem_file.seek(0)
+
+            s3_client = boto3.client("s3")
+            try:
+                s3_client.put_object(
+                    Body=in_mem_file,
+                    Bucket="my-whiskies-pics",
+                    Key=f"{new_filename}.png",
+                    ContentType="image/png",
+                )
+            except ClientError:
+                # TODO: log error
+                return False
+
+    return True
+
+
+def bottle_edit_images(form: BottleEditForm, bottle: Bottle):
+    s3_client = boto3.client("s3")
+
+    for i in range(1, 4):
+        if form[f"remove_image_{i}"].data:
+            s3_client.copy_object(
+                Bucket="my-whiskies-pics",
+                CopySource=f"my-whiskies-pics/{bottle.id}_{i}.png",
+                Key=f"__del_{bottle.id}_{i}.png",
+                ContentType="image/png",
+            )
+            s3_client.delete_object(
+                Bucket="my-whiskies-pics", Key=f"{bottle.id}_{i}.png"
+            )
+
+    images = s3_client.list_objects(Bucket="my-whiskies-pics", Prefix=bottle.id).get(
+        "Contents", []
+    )
+    images.sort(key=lambda obj: obj.get("Key"))
+
+    for idx, img in enumerate(images, 1):
+        img_num = int(img.get("Key").split("_")[-1].split(".")[0])
+
+        if idx != img_num:
+            s3_client.copy_object(
+                Bucket="my-whiskies-pics",
+                CopySource=f"my-whiskies-pics/{bottle.id}_{img_num}.png",
+                Key=f"{bottle.id}_{idx}.png",
+                ContentType="image/png",
+            )
+            s3_client.delete_object(
+                Bucket="my-whiskies-pics", Key=f"{bottle.id}_{img_num}.png"
+            )
+
+
+def bottle_delete_images(bottle: Bottle):
+    s3_client = boto3.client("s3")
+    for i in range(1, 4):
+        s3_client.delete_object(
+            Bucket="my-whiskies-pics", Key=f"__del_{bottle.id}_{i}.png"
+        )
+
+
+def get_bottle_image_count(bottle_id: str) -> int:
+    s3_client = boto3.client("s3")
+    images = s3_client.list_objects(Bucket="my-whiskies-pics", Prefix=bottle_id).get(
+        "Contents", []
+    )
+    return len(images)
