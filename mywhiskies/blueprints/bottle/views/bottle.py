c5f7e2161f47f981fb855c401e6ada550af4116a
Charlie Griefer <charlie@griefer.com>
2024-11-01 01:23:26 -0700

    For #157 - completed the refactor of bottlers

--- mywhiskies/blueprints/bottle/views/bottle.py
+++ mywhiskies/blueprints/bottle/views/bottle.py
@@ -1,15 +1,14 @@
 import time
-from datetime import datetime
 
-from dateutil.relativedelta import relativedelta
-from flask import make_response, redirect, render_template, request, url_for
+from flask import redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from mywhiskies.blueprints.bottle import bottle_bp
 from mywhiskies.blueprints.bottle.forms import BottleEditForm, BottleForm
-from mywhiskies.blueprints.bottle.models import Bottle, BottleTypes
+from mywhiskies.blueprints.bottle.models import Bottle
 from mywhiskies.blueprints.user.models import User
 from mywhiskies.extensions import db
+from mywhiskies.services import utils
 from mywhiskies.services.bottle.bottle import (
     add_bottle,
     delete_bottle,
@@ -27,32 +26,10 @@ from mywhiskies.services.bottle.image import get_s3_config
     strict_slashes=False,
 )
 def bottles(username: str):
-    dt_list_length = request.cookies.get("dt-list-length", "50")
     user = db.one_or_404(db.select(User).filter_by(username=username))
-
-    bottles_to_list, active_bottle_types, is_my_list, killed_bottles = list_bottles(
-        user, request
-    )
-
-    response = make_response(
-        render_template(
-            "bottle/bottle_list.html",
-            title=f"{user.username}'s Whiskies: Bottles",
-            has_datatable=True,
-            user=user,
-            bottles=bottles_to_list,
-            has_killed_bottles=bool(len(killed_bottles)),
-            bottle_types=BottleTypes,
-            active_filters=active_bottle_types,
-            dt_list_length=dt_list_length,
-            is_my_list=is_my_list,
-        )
-    )
-
-    response.set_cookie(
-        "dt-list-length",
-        value=dt_list_length,
-        expires=datetime.now() + relativedelta(years=1),
+    response = list_bottles(user, request, current_user)
+    utils.set_cookie_expiration(
+        response, "dt_list_length", request.cookies.get("dt-list-length", "50")
     )
     return response
 
@@ -78,11 +55,9 @@ def bottle_detail(bottle_id: str):
 @login_required
 def bottle_add():
     form = prepare_bottle_form(current_user, BottleForm())
-
     if form.validate_on_submit():
         add_bottle(form, current_user)
-        return redirect(url_for("core.home", username=current_user.username.lower()))
-
+        return redirect(url_for("core.home", username=current_user.username))
     return render_template(
         "bottle/bottle_add.html",
         title=f"{current_user.username}'s Whiskies: Add Bottle",
@@ -101,7 +76,7 @@ def bottle_edit(bottle_id: str):
 
     if form.validate_on_submit():
         edit_bottle(form, _bottle)
-        return redirect(url_for("core.home", username=current_user.username.lower()))
+        return redirect(url_for("core.home", username=current_user.username))
 
     return render_template(
         "bottle/bottle_edit.html",
@@ -116,6 +91,4 @@ def bottle_edit(bottle_id: str):
 @login_required
 def bottle_delete(bottle_id: str):
     delete_bottle(bottle_id)
-    return redirect(
-        url_for("bottle.list_bottles", username=current_user.username.lower())
-    )
+    return redirect(url_for("bottle.list_bottles", username=current_user.username))
