863986edacb60f4ed9755692c1987591afd2aba5
Charlie Griefer <charlie@griefer.com>
2024-11-01 03:13:46 -0700

    For #155 - just added some type hinting

--- mywhiskies/app.py
+++ mywhiskies/app.py
@@ -10,7 +10,6 @@ from mywhiskies.blueprints.bottler import bottler_bp
 from mywhiskies.blueprints.core.views import core
 from mywhiskies.blueprints.distillery import distillery_bp
 from mywhiskies.blueprints.errors.views import errors
-from mywhiskies.blueprints.user.views import user
 from mywhiskies.extensions import register_extensions
 
 dotenv_path = os.path.join(
@@ -19,13 +18,7 @@ dotenv_path = os.path.join(
 load_dotenv(dotenv_path=dotenv_path, verbose=True)
 
 
-def create_app(settings_override=None):
-    """
-    Create a Flask app using the app factory pattern.
-
-    :param settings_override: Override settings
-    :return: Flask app
-    """
+def create_app(settings_override: dict = None) -> Flask:
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object(os.environ["CONFIG_TYPE"])
@@ -43,7 +36,6 @@ def create_app(settings_override=None):
     app.register_blueprint(bottler_bp)
     app.register_blueprint(distillery_bp)
     app.register_blueprint(errors)
-    app.register_blueprint(user)
     register_extensions(app)
 
     return app
