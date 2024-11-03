479271c008fb27456c1d53fcc2415062483035b8
Charlie Griefer <charlie@griefer.com>
2024-11-03 01:52:07 -0700

    For #155 - some code organization and cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import sys
 
 import pytest
 
+from mywhiskies.blueprints.user.models import User
+
 # Ensure the parent directory is added to the system path before imports
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -11,6 +13,8 @@ from mywhiskies.app import create_app  # noqa: E402
 
 # from mywhiskies.blueprints.user.models import User  # noqa: E402
 from mywhiskies.extensions import db  # noqa: E402
+
+TEST_PASSWORD = "testpass"
 
 
 @pytest.fixture(scope="session")
@@ -25,12 +29,25 @@ def app():
         db.drop_all()
 
 
-# @pytest.fixture
-# def test_user(app):
-#     """Create a test user."""
-#     with app.app_context():
-#         user = User(username="testuser", email="test@example.com")
-#         user.set_password("testpass")
-#         db.session.add(user)
-#         db.session.commit()  # This will go to the test database
-#     return user
+@pytest.fixture
+def test_user(app):
+    with app.app_context():
+        # delete any existing users with the same username or email
+        db.session.execute(db.delete(User).where(User.username == "testuser"))
+        db.session.execute(db.delete(User).where(User.email == "test@example.com"))
+        db.session.commit()
+
+        user = User(
+            username="testuser",
+            email="test@example.com",
+            email_confirmed=True,
+        )
+        user.set_password(TEST_PASSWORD)
+        db.session.add(user)
+        db.session.commit()
+    return user
+
+
+@pytest.fixture
+def login_data(test_user):
+    return {"username": test_user.username, "password": TEST_PASSWORD}
--- tests/integration/test_reset_password_request_form.py
+++ tests/integration/test_reset_password_request_form.py
@@ -1,37 +1,11 @@
-import pytest
 from werkzeug.datastructures import MultiDict
 
 from mywhiskies.blueprints.auth.forms import ResetPasswordRequestForm
-from mywhiskies.blueprints.user.models import User
-from mywhiskies.extensions import db
-
-
-@pytest.fixture
-def test_user(app):
-    """Create a test user."""
-    with app.app_context():
-
-        # Delete any existing users with the same username or email
-        db.session.execute(db.delete(User).where(User.username == "testuser"))
-        db.session.execute(db.delete(User).where(User.email == "test@example.com"))
-        db.session.commit()
-
-        # Create the new test user
-        user = User(
-            username="testuser",
-            email="test@example.com",
-            email_confirmed=True,
-        )
-        user.set_password("testpass")
-        db.session.add(user)
-        db.session.commit()
-    return user
 
 
 def test_reset_password_request_form_valid(app, test_user):
     formdata = MultiDict(
         {
-            "form_name": "reset_pw_request",
             "email": test_user.email,
         }
     )
