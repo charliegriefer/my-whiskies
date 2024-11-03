5a1c09d445024938385ddc6a02f3e3fd3848cbb2
Charlie Griefer <charlie@griefer.com>
2024-11-03 00:30:35 -0700

    For #155 - updated app() function to avoid detached sessions during testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,36 @@
 import os
+import sys
 
 import pytest
 
-from app import create_app
-from app.extensions import db
+# Ensure the parent directory is added to the system path before imports
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from config import TestConfig  # noqa: E402
+from mywhiskies.app import create_app  # noqa: E402
+
+# from mywhiskies.blueprints.user.models import User  # noqa: E402
+from mywhiskies.extensions import db  # noqa: E402
 
 
-@pytest.fixture
-def test_app():
-    os.environ["CONFIG_TYPE"] = "config.TestConfig"
-
-    test_app = create_app()
-
-    with test_app.app_context():
+@pytest.fixture(scope="session")
+def app():
+    app = create_app(config_class=TestConfig)
+    app.teardown_bkp = app.teardown_appcontext_funcs
+    app.teardown_appcontext_funcs = []
+    with app.app_context():
         db.create_all()
-        yield test_app
+        yield app
         db.session.remove()
         db.drop_all()
 
 
-@pytest.fixture
-def client(test_app):
-    return test_app.test_client()
-
-
-@pytest.fixture
-def runner(test_app):
-    return test_app.test_cli_runner()
+# @pytest.fixture
+# def test_user(app):
+#     """Create a test user."""
+#     with app.app_context():
+#         user = User(username="testuser", email="test@example.com")
+#         user.set_password("testpass")
+#         db.session.add(user)
+#         db.session.commit()  # This will go to the test database
+#     return user
