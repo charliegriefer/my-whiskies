a967e7daa9deac2097be6108ebb78d3e4b996bcb
Charlie Griefer <charlie@griefer.com>
2024-11-06 00:56:20 -0700

    For #155 - bottler edit tests done

---
+++ tests/integration/bottler/test_bottler_edit.py
@@ -0,0 +1,69 @@
+from flask import Flask, url_for
+
+from mywhiskies.blueprints.bottler.models import Bottler
+from mywhiskies.blueprints.user.models import User
+from mywhiskies.extensions import db
+from tests.conftest import TEST_PASSWORD
+
+
+def test_edit_bottler_requires_login(app: Flask, test_user_bottler: Bottler):
+    with app.test_client() as client:
+        response = client.get(
+            url_for("bottler.bottler_edit", bottler_id=test_user_bottler.id),
+            follow_redirects=False,
+        )
+        assert response.status_code == 302
+        assert url_for("auth.login", _external=False) in response.headers["Location"]
+
+
+def test_valid_bottler_edit_form(
+    app: Flask, test_user: User, test_user_bottler: Bottler
+) -> None:
+    with app.test_client() as client:
+        # Log in the test user
+        client.post(
+            url_for("auth.login"),
+            data={
+                "username": test_user.username,
+                "password": TEST_PASSWORD,
+            },
+        )
+
+        # Form data for updating the bottler
+        formdata = {
+            "name": "Updated Bottler Name",
+            "description": "An updated fine bottler.",
+            "region_1": "Philadelphia",
+            "region_2": "PA",
+            "url": "https://www.updatedbottler.com",
+        }
+
+        # Submit the form for editing the bottler
+        response = client.post(
+            url_for("bottler.bottler_edit", bottler_id=test_user_bottler.id),
+            data=formdata,
+            follow_redirects=True,
+        )
+
+        # Check that the response is successful and redirection occurs
+        assert response.status_code == 200
+        assert (
+            url_for("bottler.bottlers_list", username=test_user.username)
+            in response.request.url
+        )
+
+        # Check for success message in response data
+        assert (
+            bytes(
+                f'"{formdata["name"]}" has been successfully updated.', encoding="utf-8"
+            )
+            in response.data
+        )
+
+        # Get the updated bottler from the database
+        updated_bottler = db.session.get(Bottler, test_user_bottler.id)
+        assert updated_bottler.name == formdata["name"]
+        assert updated_bottler.description == formdata["description"]
+        assert updated_bottler.region_1 == formdata["region_1"]
+        assert updated_bottler.region_2 == formdata["region_2"]
+        assert updated_bottler.url == formdata["url"]
