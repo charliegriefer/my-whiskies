01f4a7ca045c7ed0dc64178ecb162db50f79007e
Charlie Griefer <charlie@griefer.com>
2024-11-07 00:48:38 -0700

    For #155 - cleaned up tests

--- tests/integration/bottle/test_bottle_edit.py
+++ tests/integration/bottle/test_bottle_edit.py
@@ -1,78 +1,79 @@
 from unittest.mock import MagicMock, patch
 
 from flask import Flask, url_for
+from flask.testing import FlaskClient
 from werkzeug.datastructures import FileStorage, MultiDict
 
 from mywhiskies.blueprints.bottle.forms import BottleEditForm
-from mywhiskies.blueprints.bottle.models import Bottle
 from mywhiskies.blueprints.user.models import User
 from mywhiskies.extensions import db
 from mywhiskies.services.bottle.bottle import edit_bottle
 from mywhiskies.services.bottle.form import prepare_bottle_form
 
 
-def test_edit_bottle_requires_login(app, test_user_bottle):
-    with app.test_client() as client:
-        response = client.get(
-            url_for("bottle.bottle_edit", bottle_id=test_user_bottle.id),
-            follow_redirects=False,
-        )
-        assert response.status_code == 302
-        assert url_for("auth.login", _external=False) in response.headers["Location"]
+def test_edit_bottle_requires_login(
+    app: Flask, client: FlaskClient, test_user: User
+) -> None:
+    response = client.get(
+        url_for("bottle.bottle_edit", bottle_id=test_user.bottles[0].id),
+        follow_redirects=False,
+    )
+    assert response.status_code == 302
+    assert url_for("auth.login", _external=False) in response.headers["Location"]
 
 
 def test_valid_bottle_edit_form(
     app: Flask,
     test_user: User,
-    test_user_bottle: Bottle,
     mock_image: str,
 ) -> None:
     """Test the validation of a valid bottle edit form with image upload."""
-    with app.app_context():
-        # Create a FileStorage object from a mock image
-        with open(mock_image, "rb") as f:
-            file_storage = FileStorage(
-                stream=f, filename="test_image.png", content_type="image/png"
-            )
-            # Create form data for editing
-            formdata = MultiDict(
-                {
-                    "name": "Updated Bottle Name",
-                    "type": "bourbon",
-                    "year_barrelled": 2021,
-                    "year_bottled": 2023,
-                    "abv": 70.0,
-                    "cost": 60.00,
-                    "stars": "4",
-                    "description": "An updated fine sample bottle.",
-                    "review": "Still excellent taste.",
-                    "distilleries": [str(d.id) for d in test_user_bottle.distilleries],
-                    "bottler_id": "0",
-                    "bottle_image_1": file_storage,
-                }
-            )
-            # Create and process the form
-            form = BottleEditForm()
-            prepare_bottle_form(test_user, form)
-            form.process(formdata)
+    test_bottle = test_user.bottles[0]
 
-            assert form.validate(), f"Form validation failed: {form.errors}"
+    # Create a FileStorage object from a mock image
+    with open(mock_image, "rb") as f:
+        file_storage = FileStorage(
+            stream=f, filename="test_image.png", content_type="image/png"
+        )
+        # Create form data for editing
+        formdata = MultiDict(
+            {
+                "name": "Updated Bottle Name",
+                "type": "bourbon",
+                "year_barrelled": 2021,
+                "year_bottled": 2023,
+                "abv": 70.0,
+                "cost": 60.00,
+                "stars": "4",
+                "description": "An updated fine sample bottle.",
+                "review": "Still excellent taste.",
+                "distilleries": [str(d.id) for d in test_bottle.distilleries],
+                "bottler_id": "0",
+                "bottle_image_1": file_storage,
+            }
+        )
+        # Create and process the form
+        form = BottleEditForm()
+        prepare_bottle_form(test_user, form)
+        form.process(formdata)
 
-            with patch("boto3.client") as mock_boto_client, patch(
-                "PIL.Image.open"
-            ) as mock_image_open:
-                mock_image_obj = MagicMock()
-                mock_image_open.return_value = mock_image_obj
-                mock_image_obj.width = 800
-                mock_image_obj.height = 600
+        assert form.validate(), f"Form validation failed: {form.errors}"
 
-                mock_s3_client = MagicMock()
-                mock_boto_client.return_value = mock_s3_client
+        with patch("boto3.client") as mock_boto_client, patch(
+            "PIL.Image.open"
+        ) as mock_image_open:
+            mock_image_obj = MagicMock()
+            mock_image_open.return_value = mock_image_obj
+            mock_image_obj.width = 800
+            mock_image_obj.height = 600
 
-                # Ensure the distillery object is attached to the session
-                test_user_bottle.distilleries = [
-                    db.session.merge(d) for d in test_user_bottle.distilleries
-                ]
+            mock_s3_client = MagicMock()
+            mock_boto_client.return_value = mock_s3_client
 
-                # Call the edit bottle function
-                edit_bottle(form, test_user_bottle)
+            # Ensure the distillery object is attached to the session
+            test_user.bottles[0].distilleries = [
+                db.session.merge(d) for d in test_bottle.distilleries
+            ]
+
+            # Call the edit bottle function
+            edit_bottle(form, test_bottle)
--- tests/integration/bottle/test_bottle_list.py
+++ tests/integration/bottle/test_bottle_list.py
@@ -1,70 +1,71 @@
 from flask import Flask, url_for
+from flask.testing import FlaskClient
 
-from mywhiskies.blueprints.bottle.models import Bottle
 from mywhiskies.blueprints.user.models import User
 from tests.conftest import TEST_USER_PASSWORD, html_encode
 
 
-def test_bottle_list(app: Flask, test_user: User):
-    with app.test_client() as client:
-        response = client.get(
-            url_for("bottle.list_bottles", username=test_user.username)
-        )
-        assert response.status_code == 200
-        suffix = "'s" if not test_user.username.endswith("s") else "'"
-        expected_title = html_encode(f"{test_user.username}{suffix} Whiskies")
-        assert expected_title.encode("utf-8") in response.data
-        for bottle in test_user.bottles:
-            assert bottle.name.encode("utf-8") in response.data
-            assert bottle.type.name.encode("utf-8") in response.data
-            if bottle.abv:
-                assert str(bottle.abv).encode("utf-8") in response.data
-            assert bottle.description.encode("utf-8") in response.data
+def test_bottle_list(app: Flask, client: FlaskClient, test_user: User) -> None:
+    response = client.get(url_for("bottle.list_bottles", username=test_user.username))
+
+    assert response.status_code == 200
+
+    suffix = "'s" if not test_user.username.endswith("s") else "'"
+    expected_title = html_encode(f"{test_user.username}{suffix} Whiskies")
+
+    response_data = response.get_data(as_text=True)
+
+    assert expected_title in response_data
+    for bottle in test_user.bottles:
+        assert bottle.name in response_data
+        assert bottle.type.name in response_data
+        if bottle.abv:
+            assert str(bottle.abv) in response_data
+        assert bottle.description in response_data
 
 
 def test_bottle_list_logged_in_elements(
-    app: Flask, test_user: User, npc_user: User, test_user_bottle: Bottle
-):
-    with app.test_client() as client:
-        # log in the test user
-        client.post(
-            url_for("auth.login"),
-            data={
-                "username": test_user.username,
-                "password": TEST_USER_PASSWORD,
-            },
-        )
+    app: Flask,
+    client: FlaskClient,
+    test_user: User,
+    npc_user: User,
+) -> None:
+    # log in the test user
+    client.post(
+        url_for("auth.login"),
+        data={
+            "username": test_user.username,
+            "password": TEST_USER_PASSWORD,
+        },
+    )
 
-        # get the bottle list page for another user.
-        # even though we're logged in, we shouldn't see edit or delete icons in another user's list.
-        response = client.get(
-            url_for("bottle.list_bottles", username=npc_user.username)
-        )
-        assert response.status_code == 200
-        assert b"Random Bottle" not in response.data
-        assert b"bi-pencil" not in response.data
-        assert b"bi-trash" not in response.data
+    # get the bottle list page for another user.
+    # even though we're logged in, we shouldn't see edit or delete icons in another user's list.
+    response = client.get(url_for("bottle.list_bottles", username=npc_user.username))
+    response_data = response.get_data(as_text=True)
+    assert response.status_code == 200
+    assert "Random Bottle" not in response_data
+    assert "bi-pencil" not in response_data
+    assert "bi-trash" not in response_data
 
-        # get the bottle list page the current user.
-        # now we should see the edit and delete iconss, as well as the "random" button.
-        response = client.get(
-            url_for("bottle.list_bottles", username=test_user.username)
-        )
-        assert response.status_code == 200
-        assert b"Random Bottle" in response.data
-        assert b"bi-pencil" in response.data
-        assert b"bi-trash" in response.data
+    # get the bottle list page the current user.
+    # now we should see the edit and delete iconss, as well as the "random" button.
+    response = client.get(url_for("bottle.list_bottles", username=test_user.username))
+    response_data = response.get_data(as_text=True)
+    assert response.status_code == 200
+    assert "Random Bottle" in response_data
+    assert "bi-pencil" in response_data
+    assert "bi-trash" in response_data
 
 
-def test_bottle_list_logged_out_elements(app: Flask):
-    with app.test_client() as client:
-        # not logged in, so we shouldn't see the random bottle button, or any edit or delete icons in any lists.
-        response = client.get(url_for("bottle.list_bottles", username="testuser"))
-        assert b"Random Bottle" not in response.data
-        assert b"bi-pencil" not in response.data
-        assert b"bi-trash" not in response.data
+def test_bottle_list_logged_out_elements(app: Flask, client: FlaskClient) -> None:
+    # not logged in, so we shouldn't see the random bottle button, or any edit or delete icons in any lists.
+    response = client.get(url_for("bottle.list_bottles", username="testuser"))
+    assert b"Random Bottle" not in response.data
+    assert b"bi-pencil" not in response.data
+    assert b"bi-trash" not in response.data
 
-        response = client.get(url_for("bottle.list_bottles", username="testuser"))
-        assert b"Random Bottle" not in response.data
-        assert b"bi-pencil" not in response.data
-        assert b"bi-trash" not in response.data
+    response = client.get(url_for("bottle.list_bottles", username="testuser"))
+    assert b"Random Bottle" not in response.data
+    assert b"bi-pencil" not in response.data
+    assert b"bi-trash" not in response.data
