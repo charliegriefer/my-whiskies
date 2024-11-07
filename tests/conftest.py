e2adce991415c9c012b7574d54c54ac3b07af595
Charlie Griefer <charlie@griefer.com>
2024-11-07 03:03:11 -0700

    For #155 - small code cleanup of conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ from mywhiskies.extensions import db  # noqa: E402
 TEST_USER_PASSWORD = "testpass"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def app() -> Flask:
     app = create_app(config_class=TestConfig)
     app.teardown_bkp = app.teardown_appcontext_funcs
@@ -32,11 +32,6 @@ def app() -> Flask:
         yield app
         db.session.remove()
         db.drop_all()
-
-
-@pytest.fixture
-def client(app: Flask) -> FlaskClient:
-    return app.test_client()
 
 
 @pytest.fixture(scope="function", autouse=True)
