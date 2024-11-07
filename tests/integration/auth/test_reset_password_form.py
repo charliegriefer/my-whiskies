14cf06c11173b8bc8f79865ff2c67800a1a7ecf9
Charlie Griefer <charlie@griefer.com>
2024-11-07 03:21:12 -0700

    For #155 - final cleanup of auth tests

--- tests/integration/auth/test_reset_password_form.py
+++ tests/integration/auth/test_reset_password_form.py
@@ -1,7 +1,6 @@
 import pytest
 from flask import Flask, url_for
 from flask.testing import FlaskClient
-from flask_login import logout_user
 from sqlalchemy import select
 from werkzeug.datastructures import MultiDict
 
@@ -19,57 +18,43 @@ def reset_token(test_user: User) -> str:
 def test_reset_password_valid_token(
     app: Flask, client: FlaskClient, reset_token: str, test_user: User
 ) -> None:
-    logout_user()
-    response = client.post(
-        url_for("auth.reset_password", token=reset_token),
-        data={
-            "password": "NewPassword123",
-            "password2": "NewPassword123",
-        },
-        follow_redirects=True,
-    )
-    assert response.status_code == 200
-    assert b"Your password has been reset" in response.data
+    with app.app_context():
+        response = client.post(
+            url_for("auth.reset_password", token=reset_token),
+            data={"password": "NewPassword123", "password2": "NewPassword123"},
+            follow_redirects=True,
+        )
+        assert response.status_code == 200
+        assert "Your password has been reset" in response.get_data(as_text=True)
 
-    # verify the password has been updated
-    user = (
-        db.session.execute(select(User).filter_by(username=test_user.username))
-        .scalars()
-        .first()
-    )
-    assert user.check_password("NewPassword123")
-    assert not user.check_password(TEST_USER_PASSWORD)
+        # verify the password has been updated
+        user = (
+            db.session.execute(select(User).filter_by(username=test_user.username))
+            .scalars()
+            .first()
+        )
+        assert user.check_password("NewPassword123")
+        assert not user.check_password(TEST_USER_PASSWORD)
 
 
-def test_valid_password_reset(app: Flask) -> None:
-    formdata = MultiDict(
-        {
-            "password": "NewPassword123",
-            "password2": "NewPassword123",
-        }
-    )
+def test_valid_password_reset() -> None:
+    formdata = MultiDict({"password": "NewPassword123", "password2": "NewPassword123"})
     form = ResetPWForm(formdata)
     assert form.validate()
 
 
 def assert_invalid_password_reset(
-    app: Flask, password: str, password2: str, expected_error: str, field_name: str
+    password: str, password2: str, expected_error: str, field_name: str
 ) -> None:
-    formdata = MultiDict(
-        {
-            "password": password,
-            "password2": password2,
-        }
-    )
+    formdata = MultiDict({"password": password, "password2": password2})
     form = ResetPWForm(formdata)
     assert not form.validate()
     assert field_name in form.errors
     assert expected_error in form.errors[field_name]
 
 
-def test_invalid_password_reset_different_passwords(app: Flask) -> None:
+def test_invalid_password_reset_different_passwords() -> None:
     assert_invalid_password_reset(
-        app,
         "NewPassword123",
         "DifferentPassword123",
         "Passwords do not match.",
@@ -77,9 +62,8 @@ def test_invalid_password_reset_different_passwords(app: Flask) -> None:
     )
 
 
-def test_invalid_password_reset_password_too_short(app: Flask) -> None:
+def test_invalid_password_reset_password_too_short() -> None:
     assert_invalid_password_reset(
-        app,
         "Short1",
         "Short1",
         "Field must be between 8 and 24 characters long.",
@@ -87,9 +71,8 @@ def test_invalid_password_reset_password_too_short(app: Flask) -> None:
     )
 
 
-def test_invalid_password_reset_password_contains_space(app: Flask) -> None:
+def test_invalid_password_reset_password_contains_space() -> None:
     assert_invalid_password_reset(
-        app,
         "Password 1234",
         "Password 1234",
         "Password is invalid.",
@@ -97,9 +80,8 @@ def test_invalid_password_reset_password_contains_space(app: Flask) -> None:
     )
 
 
-def test_invalid_password_reset_password_no_lowercase(app: Flask) -> None:
+def test_invalid_password_reset_password_no_lowercase() -> None:
     assert_invalid_password_reset(
-        app,
         "PASSWORD1234",
         "PASSWORD1234",
         "Password is invalid.",
@@ -107,9 +89,8 @@ def test_invalid_password_reset_password_no_lowercase(app: Flask) -> None:
     )
 
 
-def test_invalid_password_reset_password_no_uppercase(app: Flask) -> None:
+def test_invalid_password_reset_password_no_uppercase() -> None:
     assert_invalid_password_reset(
-        app,
         "password1234",
         "password1234",
         "Password is invalid.",
@@ -117,9 +98,8 @@ def test_invalid_password_reset_password_no_uppercase(app: Flask) -> None:
     )
 
 
-def test_invalid_password_reset_password_no_numbers(app: Flask) -> None:
+def test_invalid_password_reset_password_no_numbers() -> None:
     assert_invalid_password_reset(
-        app,
         "PasswordABC",
         "PasswordABC",
         "Password is invalid.",
