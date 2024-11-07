cf971756b9b14ab7f39ba698648e79c4d81386d1
Charlie Griefer <charlie@griefer.com>
2024-11-06 19:07:59 -0700

    For #155 - updated tests file

--- tests/integration/auth/test_login_form.py
+++ tests/integration/auth/test_login_form.py
@@ -1,79 +1,70 @@
-from flask import url_for
+from flask import Flask, url_for
+from flask.testing import FlaskClient
 from werkzeug.datastructures import MultiDict
 
 from mywhiskies.blueprints.auth.forms import LoginForm
+from mywhiskies.blueprints.user.models import User
+from tests.conftest import TEST_USER_PASSWORD
 
 
-def test_valid_login_form(app, login_data: dict):
+def test_valid_login_form(app: Flask, test_user: User) -> None:
     """Test that a valid login form passes validation."""
     formdata = MultiDict(
-        {
-            "username": login_data["username"],
-            "password": login_data["password"],
-        }
+        {"username": test_user.username, "password": TEST_USER_PASSWORD}
     )
     form = LoginForm(formdata)
     assert form.validate()
 
 
-def test_empty_username(app, login_data):
+def test_empty_username(app: Flask) -> None:
     """Test that an empty username fails validation."""
     formdata = MultiDict(
         {
             "username": "",
-            "password": login_data["password"],
+            "password": TEST_USER_PASSWORD,
         }
     )
     form = LoginForm(formdata)
-    assert not form.validate()  # Ensure validation fails
+    assert not form.validate()
     assert "username" in form.errors
 
 
-def test_empty_password(app, login_data):
+def test_empty_password(app: Flask, test_user: User) -> None:
     """Test that an empty password fails validation."""
     formdata = MultiDict(
         {
-            "username": login_data["username"],
+            "username": test_user.username,
             "password": "",
         }
     )
     form = LoginForm(formdata)
-    assert not form.validate()  # Ensure validation fails
+    assert not form.validate()
     assert "password" in form.errors
 
 
-def test_invalid_login(app, login_data):
+def test_invalid_login(app: Flask, client: FlaskClient, test_user: User) -> None:
     """Test that incorrect credentials fail the login process."""
-    with app.test_client() as client:
-        response = client.post(
-            url_for("auth.login"),
-            data={
-                "username": login_data["username"],
-                "password": "wrongpassword",
-            },
-            follow_redirects=True,  # Follow redirects to capture the final response
-        )
-        assert response.status_code == 200  # Ensure login page is returned again
-        assert b"Incorrect username or password!" in response.data
+    response = client.post(
+        url_for("auth.login"),
+        data={"username": test_user.username, "password": "wrongpassword"},
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert "Incorrect username or password!" in response.get_data(as_text=True)
 
 
-def test_valid_login(app, test_user, login_data):
+def test_valid_login(
+    app: Flask,
+    client: FlaskClient,
+    test_user: User,
+):
     """Test that correct credentials allow the user to log in."""
-    with app.test_client() as client:
-        response = client.post(
-            url_for("auth.login"),
-            data={
-                "username": login_data["username"],
-                "password": login_data["password"],
-            },
-            follow_redirects=True,
-        )
-        assert response.status_code == 200  # Ensure the request was successful
+    response = client.post(
+        url_for("auth.login"),
+        data={"username": test_user.username, "password": TEST_USER_PASSWORD},
+        follow_redirects=True,
+    )
 
-        # Check if the user is redirected to the home page
-        assert (
-            f"{test_user.username}&#39;s Whiskies".encode() in response.data
-        )  # Check for the user's home page content
-
-        # Check for the presence of the logout button
-        assert f"Log Out {test_user.username}".encode() in response.data
+    assert response.status_code == 200
+    assert f"{test_user.username}&#39;s Whiskies" in response.get_data(as_text=True)
+    assert f"Log Out {test_user.username}" in response.get_data(as_text=True)
