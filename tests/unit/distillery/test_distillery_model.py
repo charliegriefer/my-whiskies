5224dc55ded43df13d05a5e8a90732d0d9fff419
Charlie Griefer <charlie@griefer.com>
2024-11-12 04:21:17 -0700

    For #155 - updated distillery unit tests

--- tests/unit/distillery/test_distillery_model.py
+++ tests/unit/distillery/test_distillery_model.py
@@ -1,43 +1,20 @@
-import pytest
-
 from mywhiskies.blueprints.distillery.models import Distillery
 from mywhiskies.blueprints.user.models import User
-from mywhiskies.extensions import db
 
 
-@pytest.fixture
-def test_user(app):
-    user = User(username="testuser", email="test@example.com")
-    user.set_password("testpassword")
-    db.session.add(user)
-    db.session.commit()
-    yield user
-    db.session.delete(user)
-    db.session.commit()
-
-
-@pytest.fixture
-def test_distillery(app, test_user):
-    distillery = Distillery(
-        name="Test Distillery",
-        region_1="Region 1",
-        region_2="Region 2",
-        user_id=test_user.id,
-    )
-    db.session.add(distillery)
-    db.session.commit()
-    yield distillery
-    db.session.delete(distillery)
-    db.session.commit()
-
-
-def test_distillery_creation(test_distillery):
+def test_distillery_creation(test_distillery: Distillery) -> None:
     assert test_distillery.id is not None
-    assert test_distillery.name == "Test Distillery"
-    assert test_distillery.region_1 == "Region 1"
-    assert test_distillery.region_2 == "Region 2"
+    assert test_distillery.name == "Whiskey Del Bac"
+    assert test_distillery.description == (
+        "Every whiskey from Whiskey Del Bac is handcrafted, non-sourced, "
+        "aged in the desert and bottled in Tucson, Arizona with a love of whiskey and the desert."
+    )
+    assert test_distillery.region_1 == "Tucson"
+    assert test_distillery.region_2 == "AZ"
 
 
-def test_distillery_user_relationship(test_distillery, test_user):
-    assert test_distillery.user == test_user
-    assert test_user.distilleries[0] == test_distillery
+def test_distillery_user_relationship(
+    test_distillery: Distillery, test_user_01: User
+) -> None:
+    assert test_distillery.user == test_user_01
+    assert test_distillery in test_user_01.distilleries
--- tests/unit/distillery/test_distillery_service.py
+++ tests/unit/distillery/test_distillery_service.py
@@ -1,8 +1,7 @@
+import copy
 from datetime import datetime
 from unittest.mock import MagicMock, patch
 
-import pytest
-from flask import Flask
 from flask.testing import FlaskClient
 from werkzeug.datastructures import MultiDict
 
@@ -20,114 +19,69 @@ from mywhiskies.services.distillery.distillery import (
 )
 
 
-@pytest.fixture
-def test_user(app: Flask) -> User:
-    user = User(username="testuser", email="test@example.com")
-    user.set_password("testPassword1234")
-    db.session.add(user)
-    db.session.commit()
-    yield user
-    # delete all distilleries associated with the user before deleting the user
-    distilleries = (
-        db.session.execute(db.select(Distillery).where(Distillery.user_id == user.id))
-        .scalars()
-        .all()
-    )
-    [db.session.delete(distillery) for distillery in distilleries]
-    db.session.commit()
-
-
-@pytest.fixture
-def test_distillery(app: Flask, test_user: User) -> Distillery:
-    distillery = Distillery(
-        name="Frey Ranch",
-        description="A ground-to-glass distillery in northern Nevada",
-        region_1="Falon",
-        region_2="MV",
-        url="https://www.freyranch.edu",
-        user_id=test_user.id,
-    )
-    db.session.add(distillery)
-    db.session.commit()
-    yield distillery
-    # delete all bottles associated with the distillery before deleting the distllery
-    distilleries = (
-        db.session.execute(
-            db.select(Distillery).where(Distillery.user_id == test_user.id)
-        )
-        .scalars()
-        .all()
-    )
-    [db.session.delete(distillery) for distillery in distilleries]
-    # only attempt to delete if the distillery still exists
-    if db.session.get(Distillery, distillery.id):
-        db.session.delete(distillery)
-        db.session.commit()
-
-
 @patch("mywhiskies.services.distillery.distillery.render_template")
 def test_list_distilleries(
-    mock_render_template: MagicMock, test_user: User, client: FlaskClient
+    mock_render_template: MagicMock, test_user_01: User, client: FlaskClient
 ) -> None:
     mock_render_template.return_value = "Rendered Template"
-    response = list_distilleries(test_user, test_user)
+    response = list_distilleries(test_user_01, test_user_01)
     assert response.data == b"Rendered Template"
 
 
 @patch("mywhiskies.services.distillery.distillery.flash")
-def test_add_distillery(mock_flash: MagicMock, test_user: User) -> None:
+def test_add_distillery(mock_flash: MagicMock, test_user_01: User) -> None:
     form_data = MultiDict(
         {
-            "name": "Ironroot Republic",
-            "region_1": "Denison",
-            "region_2": "TX",
-            "url": "https://www.ironrootrepublic.com",
+            "name": "Jack Daniel's",
+            "description": (
+                "Crafting something that endures for over 150 years takes time and character. "
+                "You’ll find plenty of both in the people and history that make Jack Daniel’s."
+            ),
+            "region_1": "Lynchburg",
+            "region_2": "TN",
+            "url": "https://www.jackdaniels.com",
         }
     )
     form = DistilleryForm(form_data)
-    add_distillery(form, test_user)
+    add_distillery(form, test_user_01)
 
     mock_flash.assert_called_once_with(
-        '"Ironroot Republic" has been successfully added.', "success"
+        '"Jack Daniel\'s" has been successfully added.', "success"
     )
 
 
 @patch("mywhiskies.services.distillery.distillery.flash")
 def test_edit_distillery(mock_flash: MagicMock, test_distillery: Distillery) -> None:
-    original_distillery = db.get_or_404(Distillery, test_distillery.id)
-    original_distillery_name = original_distillery.name
-    original_distillery_region_1 = original_distillery.region_1
-    original_distillery_region_2 = original_distillery.region_2
-    original_distillery_url = original_distillery.url
+    _distillery = db.get_or_404(Distillery, test_distillery.id)
+    original_distillery = copy.deepcopy(_distillery.__dict__)
 
     form_data = MultiDict(
         {
-            "name": "Frey Ranch Farm and Distillery and Stuff",
-            "region_1": "Fallon",
-            "region_2": "NV",
-            "url": "https://www.freyranch.com",
+            "name": "Jack Daniel's UPDATED",
+            "region_1": "Lynchburger",
+            "region_2": "TX",
+            "url": "https://www.jackdaniels.org",
         }
     )
     form = DistilleryEditForm(form_data)
     edit_distillery(form, test_distillery)
     mock_flash.assert_called_once_with(
-        '"Frey Ranch Farm and Distillery and Stuff" has been successfully updated.',
-        "success",
+        '"Jack Daniel\'s UPDATED" has been successfully updated.', "success"
     )
 
-    assert original_distillery_name != test_distillery.name
-    assert original_distillery_region_1 != test_distillery.region_1
-    assert original_distillery_region_2 != test_distillery.region_2
-    assert original_distillery_url != test_distillery.url
+    assert original_distillery.get("name") != test_distillery.name
+    assert original_distillery.get("region_1") != test_distillery.region_1
+    assert original_distillery.get("region_2") != test_distillery.region_2
+    assert original_distillery.get("url") != test_distillery.url
 
 
 @patch("mywhiskies.services.distillery.distillery.flash")
 def test_delete_distillery(
-    mock_flash: MagicMock, test_user: User, test_distillery: Distillery
+    mock_flash: MagicMock, test_user_01: User, test_distillery: Distillery
 ) -> None:
-    delete_distillery(test_distillery.id, test_user)
+    delete_distillery(test_distillery.id, test_user_01)
     mock_flash.assert_called_once_with(
-        '"Frey Ranch" has been successfully deleted.', "success"
+        '"Whiskey Del Bac" has been successfully deleted.', "success"
     )
 
 
@@ -136,7 +90,7 @@ def test_delete_distillery(
 def test_get_distillery_detail(
     mock_get_or_404: MagicMock,
     mock_is_my_list: MagicMock,
-    test_user: User,
+    test_user_01: User,
     test_distillery: Distillery,
 ) -> None:
     mock_get_or_404.return_value = test_distillery
@@ -145,20 +99,20 @@ def test_get_distillery_detail(
         name="Live Bottle",
         type=BottleTypes.bourbon,
         date_killed=None,
-        user_id=test_user.id,
+        user_id=test_user_01.id,
     )
     killed_bottle = Bottle(
         name="Killed Bottle",
         type=BottleTypes.bourbon,
         date_killed=datetime(2023, 1, 1),
-        user_id=test_user.id,
+        user_id=test_user_01.id,
     )
     test_distillery.bottles = [live_bottle, killed_bottle]
     mock_is_my_list.return_value = True
 
     # simulate a GET request
     request = MagicMock(method="GET", cookies=MultiDict({"dt-list-length": "100"}))
-    context = get_distillery_detail(test_distillery.id, request, test_user)
+    context = get_distillery_detail(test_distillery.id, request, test_user_01)
 
     # assertions for GET request
     assert (
@@ -176,7 +130,7 @@ def test_get_distillery_detail(
         cookies=MultiDict({"dt-list-length": "50"}),
         form=MultiDict({"random_toggle": "1"}),
     )
-    context = get_distillery_detail(test_distillery.id, request, test_user)
+    context = get_distillery_detail(test_distillery.id, request, test_user_01)
 
     # assertions for POST request
     assert "title" in context
