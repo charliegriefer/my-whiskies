d67b7c75598ef66ab0fb312d09b12d70e7c018ab
Charlie Griefer <charlie@griefer.com>
2024-11-12 02:10:42 -0700

    For #155 - removed unneeded fixture

--- tests/integration/bottle/conftest.py
+++ tests/integration/bottle/conftest.py
@@ -2,32 +2,6 @@ import os
 import tempfile
 
 import pytest
-from flask import Flask
-
-from mywhiskies.blueprints.bottle.models import Bottle
-from mywhiskies.blueprints.user.models import User
-from mywhiskies.extensions import db
-
-
-@pytest.fixture
-def test_user_bottle_to_delete(app: Flask, test_user: User) -> Bottle:
-    """Create a test bottle for test_user to delete."""
-    with app.app_context():
-        bottle = Bottle(
-            name="Frey Ranch Oat Whiskey",
-            type="american_whiskey",
-            year_barrelled=2018,
-            year_bottled=2022,
-            abv=52.5,
-            cost=99.00,
-            stars="2",
-            description="Not really my jam.",
-            user_id=test_user.id,
-            distilleries=[test_user.distilleries[0]],
-        )
-        db.session.add(bottle)
-        db.session.commit()
-    return bottle
 
 
 @pytest.fixture
