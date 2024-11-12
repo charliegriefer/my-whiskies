bd0ad16a27a84dd502d5d91119b3e2a7d413d81f
Charlie Griefer <charlie@griefer.com>
2024-11-12 02:11:06 -0700

    For #155 - updated bottle integration tests

--- tests/integration/bottle/test_bottle_delete.py
+++ tests/integration/bottle/test_bottle_delete.py
@@ -5,13 +5,11 @@ from mywhiskies.blueprints.bottle.models import Bottle
 from mywhiskies.blueprints.user.models import User
 
 
-def test_delete_bottle_not_logged_in(
-    client: FlaskClient,
-    test_user_bottle_to_delete: Bottle,
-) -> None:
+def test_delete_bottle_not_logged_in(client: FlaskClient, test_user_01: User) -> None:
     """Test that a user must be logged in to delete a bottle."""
+    bottle_to_delete: Bottle = test_user_01.bottles[0]
     response = client.get(
-        url_for("bottle.bottle_delete", bottle_id=test_user_bottle_to_delete.id),
+        url_for("bottle.bottle_delete", bottle_id=bottle_to_delete.id),
         follow_redirects=True,
     )
     assert response.status_code == 200
@@ -19,34 +17,32 @@ def test_delete_bottle_not_logged_in(
 
 
 def test_delete_not_my_bottle(
-    logged_in_user: FlaskClient, test_user: User, npc_user: User
+    logged_in_user: FlaskClient, test_user_01: User, test_user_02: User
 ) -> None:
     """Test that even if logged in, a user cannot delete another user's bottle."""
     client = logged_in_user
     response = client.get(
-        url_for("bottle.bottle_delete", bottle_id=npc_user.bottles[0].id),
+        url_for("bottle.bottle_delete", bottle_id=test_user_02.bottles[0].id),
         follow_redirects=True,
     )
     assert response.status_code == 200
     assert "There was an issue deleting this bottle" in response.get_data(as_text=True)
 
 
-def test_delete_my_bottle(
-    logged_in_user: FlaskClient,
-    test_user: User,
-    test_user_bottle_to_delete: Bottle,
-) -> None:
+def test_delete_my_bottle(logged_in_user: FlaskClient, test_user_01: User) -> None:
     client = logged_in_user
-    bottles_before_delete = [bottle.name for bottle in test_user.bottles]
+    bottles_before_delete = [bottle.name for bottle in test_user_01.bottles]
+
+    bottle_to_delete: Bottle = test_user_01.bottles[0]
 
     # Perform the delete operation
     response = client.get(
-        url_for("bottle.bottle_delete", bottle_id=test_user_bottle_to_delete.id),
+        url_for("bottle.bottle_delete", bottle_id=bottle_to_delete.id),
         follow_redirects=True,
     )
     assert response.status_code == 200
     assert "Bottle deleted successfully" in response.get_data(as_text=True)
 
-    bottles_after_delete = [bottle.name for bottle in test_user.bottles]
+    bottles_after_delete = [bottle.name for bottle in test_user_01.bottles]
 
     assert len(bottles_after_delete) == len(bottles_before_delete) - 1
