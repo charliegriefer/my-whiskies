5797068ebacd6ff60aadc3f5ccc524eac0beb6ce
Charlie Griefer <charlie@griefer.com>
2024-11-12 03:33:43 -0700

    For #155 - updated bottler unit tests

--- tests/unit/bottler/test_bottler_model.py
+++ tests/unit/bottler/test_bottler_model.py
@@ -1,43 +1,14 @@
-import pytest
-
 from mywhiskies.blueprints.bottler.models import Bottler
 from mywhiskies.blueprints.user.models import User
-from mywhiskies.extensions import db
 
 
-@pytest.fixture
-def test_user(app):
-    user = User(username="testuser", email="test@example.com")
-    user.set_password("testpassword")
-    db.session.add(user)
-    db.session.commit()
-    yield user
-    db.session.delete(user)
-    db.session.commit()
-
-
-@pytest.fixture
-def test_bottler(app, test_user):
-    bottler = Bottler(
-        name="Test Bottler",
-        region_1="Region 1",
-        region_2="Region 2",
-        user_id=test_user.id,
-    )
-    db.session.add(bottler)
-    db.session.commit()
-    yield bottler
-    db.session.delete(bottler)
-    db.session.commit()
-
-
-def test_bottler_creation(test_bottler):
+def test_bottler_creation(test_bottler: Bottler) -> None:
     assert test_bottler.id is not None
-    assert test_bottler.name == "Test Bottler"
-    assert test_bottler.region_1 == "Region 1"
-    assert test_bottler.region_2 == "Region 2"
+    assert test_bottler.name == "Single Cask Nation"
+    assert test_bottler.region_1 == "Guilford"
+    assert test_bottler.region_2 == "CT"
 
 
-def test_bottler_user_relationship(test_bottler, test_user):
-    assert test_bottler.user == test_user
-    assert test_user.bottlers[0] == test_bottler
+def test_bottler_user_relationship(test_bottler: Bottler, test_user_01: User) -> None:
+    assert test_bottler.user == test_user_01
+    assert test_bottler in test_user_01.bottlers
--- tests/unit/bottler/test_bottler_service.py
+++ tests/unit/bottler/test_bottler_service.py
@@ -1,8 +1,7 @@
+import copy
 from datetime import datetime
 from unittest.mock import MagicMock, patch
 
-import pytest
-from flask import Flask
 from flask.testing import FlaskClient
 from werkzeug.datastructures import MultiDict
 
@@ -20,60 +19,17 @@ from mywhiskies.services.bottler.bottler import (
 )
 
 
-@pytest.fixture
-def test_user(app: Flask) -> User:
-    user = User(username="testuser", email="test@example.com")
-    user.set_password("testPassword1234")
-    db.session.add(user)
-    db.session.commit()
-    yield user
-    # delete all bottlers associated with the user before deleting the user
-    bottlers = (
-        db.session.execute(db.select(Bottler).where(Bottler.user_id == user.id))
-        .scalars()
-        .all()
-    )
-    [db.session.delete(bottler) for bottler in bottlers]
-    db.session.commit()
-
-
-@pytest.fixture
-def test_bottler(app: Flask, test_user: User) -> Bottler:
-    bottler = Bottler(
-        name="Lost Lantern",
-        description="Some random independent bottler.",
-        region_1="Vergenes",
-        region_2="MT",
-        url="https://www.lostlantern.com",
-        user_id=test_user.id,
-    )
-    db.session.add(bottler)
-    db.session.commit()
-    yield bottler
-    # delete all bottles associated with the bottler before deleting the bottler
-    bottlers = (
-        db.session.execute(db.select(Bottler).where(Bottler.user_id == test_user.id))
-        .scalars()
-        .all()
-    )
-    [db.session.delete(bottler) for bottler in bottlers]
-    # only attempt to delete if the bottler still exists
-    if db.session.get(Bottler, bottler.id):
-        db.session.delete(bottler)
-        db.session.commit()
-
-
 @patch("mywhiskies.services.bottler.bottler.render_template")
 def test_list_bottlers(
-    mock_render_template: MagicMock, test_user: User, client: FlaskClient
+    mock_render_template: MagicMock, test_user_01: User, client: FlaskClient
 ) -> None:
     mock_render_template.return_value = "Rendered Template"
-    response = list_bottlers(test_user, test_user)
+    response = list_bottlers(test_user_01, test_user_01)
     assert response.data == b"Rendered Template"
 
 
 @patch("mywhiskies.services.bottler.bottler.flash")
-def test_add_bottler(mock_flash: MagicMock, test_user: User) -> None:
+def test_add_bottler(mock_flash: MagicMock, test_user_01: User) -> None:
     form_data = MultiDict(
         {
             "name": "Crowded Barrel Whiskey",
@@ -83,7 +39,7 @@ def test_add_bottler(mock_flash: MagicMock, test_user: User) -> None:
         }
     )
     form = BottlerAddForm(form_data)
-    add_bottler(form, test_user)
+    add_bottler(form, test_user_01)
 
     mock_flash.assert_called_once_with(
         '"Crowded Barrel Whiskey" has been successfully added.', "success"
@@ -92,38 +48,36 @@ def test_add_bottler(mock_flash: MagicMock, test_user: User) -> None:
 
 @patch("mywhiskies.services.bottler.bottler.flash")
 def test_edit_bottler(mock_flash: MagicMock, test_bottler: Bottler) -> None:
-    original_bottler = db.get_or_404(Bottler, test_bottler.id)
-    original_bottler_name = original_bottler.name
-    original_bottler_region_1 = original_bottler.region_1
-    original_bottler_region_2 = original_bottler.region_2
-    original_bottler_url = original_bottler.url
+    _bottler = db.get_or_404(Bottler, test_bottler.id)
+    original_bottler = copy.deepcopy(_bottler.__dict__)
+
     form_data = MultiDict(
         {
-            "name": "Lost Lantern Whiskey",
-            "region_1": "Vergennes",
-            "region_2": "VT",
-            "url": "https://www.lostlanternwhiskey.com",
+            "name": "Single Cask Nation UPDATED",
+            "region_1": "CT",
+            "region_2": "USA",
+            "url": "https://www.scn.com",
         }
     )
     form = BottlerEditForm(form_data)
     edit_bottler(form, test_bottler)
     mock_flash.assert_called_once_with(
-        '"Lost Lantern Whiskey" has been successfully updated.', "success"
+        '"Single Cask Nation UPDATED" has been successfully updated.', "success"
     )
 
-    assert original_bottler_name != test_bottler.name
-    assert original_bottler_region_1 != test_bottler.region_1
-    assert original_bottler_region_2 != test_bottler.region_2
-    assert original_bottler_url != test_bottler.url
+    assert original_bottler.get("name") != test_bottler.name
+    assert original_bottler.get("region_1") != test_bottler.region_1
+    assert original_bottler.get("region_2") != test_bottler.region_2
+    assert original_bottler.get("url") != test_bottler.url
 
 
 @patch("mywhiskies.services.bottler.bottler.flash")
 def test_delete_bottler(
-    mock_flash: MagicMock, test_user: User, test_bottler: Bottler
+    mock_flash: MagicMock, test_user_01: User, test_bottler: Bottler
 ) -> None:
-    delete_bottler(test_user, test_bottler.id)
+    delete_bottler(test_user_01, test_bottler.id)
     mock_flash.assert_called_once_with(
-        '"Lost Lantern" has been successfully deleted.', "success"
+        '"Single Cask Nation" has been successfully deleted.', "success"
     )
 
 
@@ -132,7 +86,7 @@ def test_delete_bottler(
 def test_get_bottler_detail(
     mock_get_or_404: MagicMock,
     mock_is_my_list: MagicMock,
-    test_user: User,
+    test_user_01: User,
     test_bottler: Bottler,
 ) -> None:
     mock_get_or_404.return_value = test_bottler
@@ -141,20 +95,20 @@ def test_get_bottler_detail(
         name="Live Bottle",
         type=BottleTypes.bourbon,
         date_killed=None,
-        user_id=test_user.id,
+        user_id=test_user_01.id,
     )
     killed_bottle = Bottle(
         name="Killed Bottle",
         type=BottleTypes.bourbon,
         date_killed=datetime(2023, 1, 1),
-        user_id=test_user.id,
+        user_id=test_user_01.id,
     )
     test_bottler.bottles = [live_bottle, killed_bottle]
     mock_is_my_list.return_value = True
 
     # simulate a GET request
     request = MagicMock(method="GET", cookies=MultiDict({"dt-list-length": "100"}))
-    context = get_bottler_detail(test_bottler.id, request, test_user)
+    context = get_bottler_detail(test_bottler.id, request, test_user_01)
 
     # assertions for GET request
     assert (
@@ -172,7 +126,7 @@ def test_get_bottler_detail(
         cookies=MultiDict({"dt-list-length": "50"}),
         form=MultiDict({"random_toggle": "1"}),
     )
-    context = get_bottler_detail(test_bottler.id, request, test_user)
+    context = get_bottler_detail(test_bottler.id, request, test_user_01)
 
     # assertions for POST request
     assert "title" in context
