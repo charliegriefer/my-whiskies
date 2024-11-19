1dd60d259ae0d22fb613b1b5ffd5d74dac92be43
Charlie Griefer <charlie@griefer.com>
2024-11-19 10:19:20 -0700

    For #118 - update some migrations

---
+++ mywhiskies/migrations/versions/fb1cec87b5fe_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fb1cec87b5fe
+Revises: 95e171c46024
+Create Date: 2024-11-19 10:17:28.922867
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fb1cec87b5fe'
+down_revision = '95e171c46024'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
