19234662e99604af2468d25e798468eb760bb2f1
Charlie Griefer <charlie@griefer.com>
2024-12-18 09:35:34 -0700

    For #176 - organized files

--- mywhiskies/services/utils.py
+++ mywhiskies/services/utils.py
@@ -93,7 +93,7 @@ def prep_datatables(
 
     response = make_response(
         render_template(
-            "shared/bottle_list.html",
+            "shared/datatable/bottles/list.html",
             title=f"{heading_01}: {heading_02}",
             heading_01=heading_01,
             heading_02=heading_02,
