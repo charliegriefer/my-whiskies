02829c6feef3079c0028abbb511837f8194e570c
Charlie Griefer <charlie@griefer.com>
2024-12-18 06:54:52 -0700

    For #176 - removed commented code

--- mywhiskies/services/bottler/bottler.py
+++ mywhiskies/services/bottler/bottler.py
@@ -59,27 +59,3 @@ def get_bottler_detail(
     bottler: Bottler, request: LocalProxy, current_user: User
 ) -> Response:
     return utils.prep_datatables(bottler, current_user, request)
-
-    # bottler = db.get_or_404(Bottler, bottler_id)
-    # bottles = bottler.bottles
-    # live_bottles = [bottle for bottle in bottles if bottle.date_killed is None]
-    # if request.method == "POST" and bool(int(request.form.get("random_toggle"))):
-    #     bottles_to_list = [random.choice(live_bottles)] if live_bottles else []
-    #     has_killed_bottles = False
-    # else:
-    #     bottles_to_list = bottles
-    #     has_killed_bottles = any(b.date_killed for b in bottles)
-
-    # context = {
-    #     "title": f"{bottler.user.username}'s Whiskies: {bottler.name}",
-    #     "has_datatable": True,
-    #     "user": bottler.user,
-    #     "is_my_list": utils.is_my_list(bottler.user.username, current_user),
-    #     "bottler": bottler,
-    #     "bottles": bottles_to_list,
-    #     "live_bottles": live_bottles,
-    #     "has_killed_bottles": has_killed_bottles,
-    #     "dt_list_length": request.cookies.get("dt-list-length", "50"),
-    # }
-
-    # return context
