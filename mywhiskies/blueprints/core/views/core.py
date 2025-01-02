3bdf91c471408cb953a00f5efe06b40914e9f053
Charlie Griefer <charlie@griefer.com>
2025-01-02 06:44:39 -0700

    For #186 - update page title based on whether or not user is logged in

--- mywhiskies/blueprints/core/views/core.py
+++ mywhiskies/blueprints/core/views/core.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import current_user
 
 from mywhiskies.blueprints.core import core_bp
 from mywhiskies.services.core.core import get_index_counts
@@ -8,10 +9,14 @@ from mywhiskies.services.core.core import get_index_counts
 @core_bp.route("/main")
 def main():
     counts = get_index_counts()
+    title = "My Whiskies Online"
+
+    if current_user.is_authenticated:
+        title += f": {current_user.username}'s Whiskies"
 
     return render_template(
         "core/main.html",
-        title="My Whiskies Online",
+        title=title,
         user_count=counts.get("user_count", 0),
         distillery_count=counts.get("distillery_count", 0),
         bottle_count=counts.get("bottle_count", 0),
