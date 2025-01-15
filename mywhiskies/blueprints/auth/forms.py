9de74eede10690cd0f20b7c90cb1777451947680
Charlie Griefer <charlie@griefer.com>
2025-01-15 04:45:33 -0700

    For #202 - moved creation of 'terms' label from service into this file (into the Form definition) and now calling find_user_by_email() from registration service

--- mywhiskies/blueprints/auth/forms.py
+++ mywhiskies/blueprints/auth/forms.py
@@ -1,10 +1,10 @@
 import re
 
 import requests
-from flask import current_app, request
+from flask import Markup, current_app, request, url_for
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, HiddenField, PasswordField, StringField, SubmitField
-from wtforms.fields import EmailField
+from wtforms.fields import EmailField, Label
 from wtforms.validators import (
     DataRequired,
     Email,
@@ -15,17 +15,18 @@ from wtforms.validators import (
 )
 
 from mywhiskies.blueprints.user.models import User
+from mywhiskies.services.auth.registration import find_user_by_email
 
 USERNAME_DESCRIPTION = (
     "Username requirements:<ul>"
     "<li>Must be between 4 and 24 characters.</li>"
-    "<li>Valid special characters: - _ @ ! ^ * $</li>"
-    "<li>Spaces are not allowed.</li></ul>"
+    "<li>Only letters, numbers, and underscore characters are allowed.</li>"
+    "</ul>"
 )
 
 PW_DESCRIPTION = (
     "Password requirements:<ul>"
-    "<li>Must be between 8 and 22 characters.</li>"
+    "<li>Must be between 8 and 24 characters.</li>"
     "<li>Must contain at least one uppercase letter, one lowercase letter, and one digit.</li>"
     "<li>Spaces are not allowed.</li></ul>"
 )
@@ -43,14 +44,31 @@ RESET_CAPTCHA_MESSAGE = (
 )
 
 
+class UsernameValidatorMixin:
+    def validate_username(self, username: StringField) -> None:
+        error_message = ""
+        m = re.compile(r"[a-zA-Z0-9_]+$")
+        if not m.match(username.data):
+            error_message = "Username does not adhere to the specified requirements."
+        if User.query.filter_by(username=username.data).first():
+            error_message = (
+                f'"{username.data}" is unavailable. Please choose a different username.'
+            )
+        if username.data != self.username and len(error_message):
+            raise ValidationError(error_message)
+
+
 class PasswordValidatorMixin:
     def validate_password(self, password: StringField) -> None:
         regex = [r"[A-Z]", r"[a-z]", r"[0-9]"]
-        is_valid_pw = all(re.search(r, password.data) for r in regex) and not re.search(
-            r"\s", password.data
+        is_valid_pw = (
+            8 <= len(password.data) <= 24
+            and all(re.search(r, password.data) for r in regex)
+            and not re.search(r"\s", password.data)
         )
-        if password.data != self.password and not is_valid_pw:
-            raise ValidationError("Password is invalid.")
+
+        if not is_valid_pw:
+            raise ValidationError("Password does not meet the defined requirements.")
 
 
 class ReCaptchaV3:
@@ -121,7 +139,10 @@ class ResetPasswordRequestForm(FlaskForm):
     form_name = HiddenField("form_name", default="reset_pw_request")
     email = EmailField(
         "Email Address:",
-        validators=[InputRequired(), Email(message="Enter a valid e-mail address.")],
+        validators=[
+            InputRequired("Email address is required."),
+            Email(message="Enter a valid e-mail address."),
+        ],
         render_kw={"placeholder": "E-Mail Address"},
     )
     recaptcha = SubmitField(validators=[ReCaptchaV3(action="submit", threshold=0.5)])
@@ -131,14 +152,14 @@ class ResetPasswordRequestForm(FlaskForm):
 class ResetPWForm(PasswordValidatorMixin, FlaskForm):
     password = PasswordField(
         "Password:",
-        validators=[InputRequired(), Length(min=8, max=24)],
+        validators=[InputRequired("Password is required.")],
         render_kw={"placeholder": "Password"},
         description=PW_DESCRIPTION,
     )
     password2 = PasswordField(
         "Repeat Password:",
         validators=[
-            InputRequired(),
+            InputRequired("Verifying the password is required."),
             EqualTo("password", message="Passwords do not match."),
         ],
         render_kw={"placeholder": "Repeat Password"},
@@ -146,7 +167,7 @@ class ResetPWForm(PasswordValidatorMixin, FlaskForm):
     submit = SubmitField("Reset My Password ")
 
 
-class RegistrationForm(PasswordValidatorMixin, FlaskForm):
+class RegistrationForm(UsernameValidatorMixin, PasswordValidatorMixin, FlaskForm):
     form_name = HiddenField("form_name", default="registration")
     username = StringField(
         "Username:",
@@ -156,12 +177,15 @@ class RegistrationForm(PasswordValidatorMixin, FlaskForm):
     )
     email = StringField(
         "Email Address:",
-        validators=[InputRequired("Email address is required."), Email()],
+        validators=[
+            InputRequired("Email address is required."),
+            Email("Please enter a valid email address."),
+        ],
         render_kw={"placeholder": "Email Address"},
     )
     password = PasswordField(
         "Password:",
-        validators=[InputRequired("Password is required."), Length(min=8, max=24)],
+        validators=[InputRequired("Password is required.")],
         render_kw={"placeholder": "Password"},
         description=PW_DESCRIPTION,
     )
@@ -174,27 +198,25 @@ class RegistrationForm(PasswordValidatorMixin, FlaskForm):
         render_kw={"placeholder": "Repeat Password"},
     )
     agree_terms = BooleanField(
-        "", validators=[DataRequired("You must agree to the Terms of Service.")]
+        "",
+        validators=[DataRequired("You must agree to the Terms of Service.")],
     )
     recaptcha = SubmitField(validators=[ReCaptchaV3(action="submit", threshold=0.5)])
     submit = SubmitField("Register")
 
-    def validate_username(self, username: StringField) -> None:
-        error_message = ""
-        m = re.compile(r"[a-zA-Z0-9-_@!^*$]*$")
-        if not m.match(username.data):
-            error_message = "Username is invalid."
-        if User.query.filter_by(username=username.data).first():
-            error_message = (
-                f'"{username.data}" is unavailable. Please choose a different username.'
-            )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_terms_label()
 
-        if username.data != self.username and len(error_message):
-            raise ValidationError(error_message)
+    def set_terms_label(self) -> None:
+        txt = Markup(
+            f"I agree to the <a href=\"{url_for('core.terms')}\">Terms of Service</a>"
+        )
+        self.agree_terms.label = Label("agree_terms", txt)
 
     def validate_email(self, email: StringField) -> None:
-        if email.data != self.email and User.query.filter_by(email=email.data).first():
-            raise ValidationError("Please use a different email address.")
+        if email.data != self.email and find_user_by_email(email.data):
+            raise ValidationError("This email address cannot be used.")
 
 
 class ResendRegEmailForm(FlaskForm):
