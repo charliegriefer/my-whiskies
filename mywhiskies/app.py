2c8005c0927ac1f91019f7c40c7d1852a3e9195c
Charlie Griefer <charlie@griefer.com>
2025-01-23 03:55:26 -0700

    For #149 - set up logging

--- mywhiskies/app.py
+++ mywhiskies/app.py
@@ -1,5 +1,7 @@
+import logging
 import os
 from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
 
 from dotenv import load_dotenv
 from flask import Flask
@@ -31,6 +33,25 @@ def create_app(settings_override: dict = None, config_class: type = None) -> Fla
 
     if settings_override:
         app.config.update(settings_override)
+
+    log_dir = os.getenv("LOG_DIR", "../log")
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Logging configuration
+    if not app.testing and os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        log_file = os.path.join(log_dir, "my-whiskies.log")
+        file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1)
+        file_handler.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
+        file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+            )
+        )
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
+    app.logger.info("App startup")
 
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 
