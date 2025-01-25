f5ce19b3f49f721095711debd9d6677dac98adc2
Charlie Griefer <charlie@griefer.com>
2025-01-25 04:17:52 -0700

    For #149 - removed manual email call on 500 error definition, and added error logging call, which will trigger the SMTP logger

--- mywhiskies/blueprints/errors/views.py
+++ mywhiskies/blueprints/errors/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, current_app, render_template, request
 
 from mywhiskies.extensions import db
-from mywhiskies.services.email.email import send_email
 
 errors = Blueprint("errors", __name__, template_folder="templates")
 
@@ -23,11 +22,7 @@ def not_found_error(error):
 @errors.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    send_email(
-        "My Whiskies Online ERROR",
-        sender=current_app.config["MAIL_SENDER"],
-        recipients=current_app.config["MAIL_ADMINS"],
-        text_body=render_template("email/error.txt", error=error),
-        html_body=render_template("email/error.html", error=error),
+    current_app.logger.error(
+        f"500 Internal Server Error at {request.path}: {error}", exc_info=True
     )
     return render_template("errors/500.html"), 500
