3556739b4866fb0469287cc32b05bc69b3f5411f
Charlie Griefer <charlie@griefer.com>
2025-04-06 18:20:30 -0700

    For #225 - move migrations folder up a level.

--- mywhiskies/extensions.py
+++ mywhiskies/extensions.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -13,7 +15,9 @@ def register_extensions(app):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    migrate.init_app(app, db)
+
+    migrations_dir = os.path.join(os.path.dirname(app.root_path), "migrations")
+    migrate.init_app(app, db, directory=migrations_dir)
 
     login_manager.login_view = "auth.login"
     login_manager.login_message = "Please log in to access this page."
