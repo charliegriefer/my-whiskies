aa1c8702f7b660c51588214ed46d1786cb856698
Charlie Griefer <charlie@griefer.com>
2025-07-27 16:03:07 -0700

    For #240 - import UserLogin

--- mywhiskies/models/__init__.py
+++ mywhiskies/models/__init__.py
@@ -2,13 +2,14 @@ from .bottle import Bottle, BottleImage, BottleTypes
 from .bottler import Bottler
 from .core import bottle_distillery
 from .distillery import Distillery
-from .user import User
+from .user import User, UserLogin
 
 __all__ = [
     "Bottle",
     "BottleImage",
     "BottleTypes",
     "User",
+    "UserLogin",
     "Distillery",
     "Bottler",
     "bottle_distillery",
