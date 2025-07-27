2b8cc3405e3455abfaad98b94d912c542d85c63b
Charlie Griefer <charlie@griefer.com>
2025-07-27 13:49:30 -0700

    For #240 - create central models index

---
+++ mywhiskies/models/__init__.py
@@ -0,0 +1,15 @@
+from .bottle import Bottle, BottleImage, BottleTypes
+from .bottler import Bottler
+from .core import bottle_distillery
+from .distillery import Distillery
+from .user import User
+
+__all__ = [
+    "Bottle",
+    "BottleImage",
+    "BottleTypes",
+    "User",
+    "Distillery",
+    "Bottler",
+    "bottle_distillery",
+]
